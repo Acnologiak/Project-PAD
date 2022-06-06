39c963b3d55a15a79e537d06c57261db22ade2a0
Artem Kholodenko <31826517+Acnologiak@users.noreply.github.com>
2022-06-06 10:02:26 +0200

    updated app.py

--- app.py
+++ app.py
@@ -4,40 +4,75 @@ import pickle
 model = pickle.load(open("model.sv",'rb'))
 
 # Diabetes_binary: min - 0.0, max - 1.0 i
-# HighBP: min - 0.0, max - 1.0 i
-# HighChol: min - 0.0, max - 1.0 i
-# CholCheck: min - 0.0, max - 1.0 i
-# BMI: min - 12.0, max - 98.0 i
-# Smoker: min - 0.0, max - 1.0 i
-# Stroke: min - 0.0, max - 1.0 i
-# HeartDiseaseorAttack: min - 0.0, max - 1.0 i
-# PhysActivity: min - 0.0, max - 1.0 i
-# Fruits: min - 0.0, max - 1.0 i
-# Veggies: min - 0.0, max - 1.0 i
-# HvyAlcoholConsump: min - 0.0, max - 1.0 i
-# AnyHealthcare: min - 0.0, max - 1.0 i
-# NoDocbcCost: min - 0.0, max - 1.0 i
-# GenHlth: min - 1.0, max - 5.0 i
-# MentHlth: min - 0.0, max - 30.0 i
-# PhysHlth: min - 0.0, max - 30.0 i
-# DiffWalk: min - 0.0, max - 1.0 i
-# Sex: min - 0.0, max - 1.0 w
-# Age: min - 1.0, max - 13.0 i
-# Education: min - 1.0, max - 6.0 i
-# Income: min - 1.0, max - 8.0 i
+yesNo = {0:"No", 1:"Yes"}
+sex = {0:"Female", 1:"Male"}
+
 
 def main():
 
-	st.set_page_config(page_title="Heart Disease")
+	st.set_page_config(page_title="Diabetes")
 	overview = st.container()
 	left, right = st.columns(2)
 	prediction = st.container()
 
-	st.image("https://health.clevelandclinic.org/wp-content/uploads/sites/3/2020/01/mildHeartAttack-866257238-770x553.jpg")
+	#st.image("https://health.clevelandclinic.org/wp-content/uploads/sites/3/2020/01/mildHeartAttack-866257238-770x553.jpg")
 
 	with overview:
-		st.title("Heart Disease")
+		st.title("Diabetes")
 
+	with left:
+		_highBP = st.radio("Do you have high blood pressure?", list(yesNo.keys()), format_func=lambda x: yesNo[x])
+		_highChol = st.radio("Do you have high cholesterol?", list(yesNo.keys()), format_func=lambda x: yesNo[x])
+		_cholCheck = st.radio("Have you checked for cholesterol in 5 years?", list(yesNo.keys()),
+							  format_func=lambda x: yesNo[x])
+		_BMI = st.slider("Your body mass index", value=22, min_value=10, max_value=100)
+		_smoker = st.radio("Have you smoked at least 100 cigarettes in your entire life?", list(yesNo.keys()),
+						   format_func=lambda x: yesNo[x])
+		_stroke = st.radio("Did you have a stroke?", list(yesNo.keys()), format_func=lambda x: yesNo[x])
+		_heartDiseaseorAttack = st.radio("Did you have a coronary heart disease or myocardial infarction?",
+										 list(yesNo.keys()), format_func=lambda x: yesNo[x])
+		_physActivity = st.radio("Have you had any physical activity in the last 30 days except work?",
+								 list(yesNo.keys()), format_func=lambda x: yesNo[x])
+		_fruits = st.radio("Do you eat fruit at least once a day?", list(yesNo.keys()), format_func=lambda x: yesNo[x])
+		_vegetables = st.radio("Do you eat vegetables at least once a day?", list(yesNo.keys()),
+							   format_func=lambda x: yesNo[x])
+		_hvyAlcoholConsump = st.radio(
+			"Do you heavy drinkers (adult men having more than 14 drinks per week and adult women having more than 7 drinks per week)?",
+			list(yesNo.keys()), format_func=lambda x: yesNo[x])
+
+	with right:
+		_anyHealthcare = st.radio(
+			"Do you have any kind of health care coverage, including health insurance, prepaid plans such as HMO?",
+			list(yesNo.keys()), format_func=lambda x: yesNo[x])
+		_noDocbcCost = st.radio(
+			"Was there a time in the past 12 months when you needed to see a doctor but could not because of cost?",
+			list(yesNo.keys()), format_func=lambda x: yesNo[x])
+		_genHlth = st.slider(
+			"Would you say that in general your health is: 1 = excellent, 2 = very good, 3 = good, 4 = fair, 5 = poor?",
+			value=2, min_value=1, max_value=5)
+		_mentHlth = st.slider(
+			"Now thinking about your mental health, which includes stress, depression, and problems with emotions, for how",
+			value=5, min_value=1, max_value=30)
+		_physHlth = st.slider(
+			"Now thinking about your physical health, which includes physical illness and injury, for how many days during the past 30",
+			value=5, min_value=1, max_value=30)
+		_diffWalk = st.radio("Do you have serious difficulty walking or climbing stairs?", list(yesNo.keys()),
+							 format_func=lambda x: yesNo[x])
+		_sex = st.radio("Sex", list(sex.keys()), format_func=lambda x: sex[x])
+		_age = st.slider("Your age in the 13-level age category", value=2, min_value=1, max_value=13)
+		_education = st.slider("Your level of education according to EDUCA", value=5, min_value=1, max_value=6)
+		_income = st.slider("How do you feel about your income level: 1 - low, 8 - high?", value=4, min_value=1,
+							max_value=8)
+
+	data = [[_highBP, _highChol, _cholCheck, _BMI, _smoker, _stroke, _heartDiseaseorAttack, _physActivity, _fruits,
+			 _vegetables, _hvyAlcoholConsump, _anyHealthcare, _noDocbcCost, _genHlth, _mentHlth, _physHlth, _diffWalk,
+			 _sex, _age, _education, _income]]
+	chances = model.predict_proba(data)
+
+	with prediction:
+		st.subheader("Chances of getting diabetes:")
+		st.write("Probability - ", round(chances[0][1]*100), "%")
 
 if __name__ == "__main__":
     main()
+
